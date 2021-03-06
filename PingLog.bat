dcf29c0af7f17c9149f4a63f5cec341c03ad8001
LoreKeeperKen <38066929+LoreKeeperKen@users.noreply.github.com>
2021-03-06 12:48:07 -0700

    Update PingLog.bat
    Add support for German and clean up

--- PingLog.bat
+++ PingLog.bat
@@ -1,9 +1,17 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal EnableDelayedExpansion
+
+
+
+
+
 
 rem ============================================================================================
-rem Intialize variables
 rem ============================================================================================
+rem = Intialize variables
+rem ============================================================================================
+rem ============================================================================================
+
 
 set hyphen=-
 set response=0
@@ -13,30 +21,70 @@ set argArray=
 set argCount=0
 
 
+rem ===== English replies ======================================================================
+set replies[0409]=Reply
+set pingRequests[0409]=Ping request
+set requests[0409]=Request
+
+
+rem ===== German replies =======================================================================
+set replies[0407]=Antwort
+set pingRequests[0407]=Ping-Anforderung
+set requests[0407]=Zeitüberschreitung
+
+
+rem When adding support for new languages use the number returned by the below reg query for the index 
+
+
+rem ==== Getting and storing system's default language =========================================
+for /f "delims=" %%a in ('reg query "hklm\system\controlset001\control\nls\language" /v Default') do @set defaultLang=%%a
+set defaultLang=%defaultLang:~-4%
+
+
+rem ==== Store string length for use in LOOP ==================================================
+call :strlen replyLength replies[!defaultLang!]
+call :strlen pingRequestLength pingRequests[!defaultLang!]
+call :strlen requestLength requests[!defaultLang!]
+
+
+
+
 
 
 rem ============================================================================================
-rem Check environment variable to see if pinglog was executed from cmd line or by clicking on the batch file. 
 rem ============================================================================================
+rem = Check environment variable to see if pinglog was executed from cmd line or by clicking 
+rem = on the batch file. 
+rem ============================================================================================
+rem ============================================================================================
+
 
 if "!cmdcmdline!"=="!cmdcmdline:%~f0=!" (
     set executefrom=cmd
 )
 
+
 if "!executefrom!"=="cmd" (
     goto CMD
 )
+
 
 goto FILE
 
 
 
 
+
+
 rem ============================================================================================
-rem If executed from cmd, interates over passed arguments, removes -* arguments, and sets IP.
+rem ============================================================================================
+rem = If executed from cmd, interates over passed arguments, removes -* arguments, and sets IP.
+rem ============================================================================================
 rem ============================================================================================
 
+
 :CMD
+
 
 for %%P in (%*) do (
     set /A argCount+=1
@@ -48,103 +96,134 @@ for %%P in (%*) do (
     )
 )
 
+
 for /l %%I in (1,1,9) do (
     set ip=!ip!!argArray[%%I]!
 )
 
+
 if "!ip!"=="" (
     set ip=%default%
     echo No IP/URL entered, using default !default!
-    goto MIDDLE
+    goto PREP
 )
 
-goto MIDDLE
+
+goto PREP
+
+
 
 
 
 
 rem ============================================================================================
-rem If executed by clicking on batch file, prompts for IP, defaults if no IP provided, and removes -* arguments.
 rem ============================================================================================
+rem = If executed by clicking on batch file, prompts for IP, defaults if no IP provided, 
+rem = and removes -* arguments.
+rem ============================================================================================
+rem ============================================================================================
+
 
 :FILE
+
 
 if "!ip!"=="" (
     set /p ip="Enter IP/URL to timeless ping: "
 )
 
+
 if "!ip!"=="" (
     set ip=%default%
     echo No IP/URL entered, using default !default!
 )
 
+
 for %%C in (
--a -c -f -i -j -k -l -n -p -r -s -t -v -w -4 
--A -C -F -I -J -K -L -N -P -R -S -T -V -W -6
+    -a -c -f -i -j -k -l -n -p -r -s -t -v -w -4 
+    -A -C -F -I -J -K -L -N -P -R -S -T -V -W -6
 ) do (
-   set ip=!ip:%%C=!
+    set ip=!ip:%%C=!
 )
 
-goto MIDDLE
+
+goto PREP
+
 
 
 
 
 
 rem ============================================================================================
-rem Preps IP and time for us and intializes filename.
+rem ============================================================================================
+rem = Preps IP and time and intializes filename.
+rem ============================================================================================
 rem ============================================================================================
 
-:MIDDLE
+
+:PREP
+
 
 set ip=%ip: =%
 set hour=%time: =0%
 set filename=ping-%ip%-%DATE:~-4,4%%DATE:~-10,2%%DATE:~-7,2%-%hour:~0,2%%hour:~3,2%.txt
+
 
 goto LOOP
 
 
 
 
+
+
 rem ============================================================================================
-rem Loops ping -n 1 to simulate timeless ping
+rem ============================================================================================
+rem = Loops ping -n 1 to simulate timeless ping
+rem ============================================================================================
 rem ============================================================================================
 
+
 :LOOP
+
 
 for /f "delims=" %%P in ('ping -n 1 %ip%') do (
     set /A i+=1
     set pingarray[!i!]=%%P
 )
 
+
 set LineCount=%i%
+
+
+rem %replyLength%!
+rem replies[%defaultLang%]
+rem replies[0409]
 
 
 rem =========== Interates over the ping reply =================================================
 for /l %%L in (1,1,%LineCount%) do (
-    if "!pingarray[%%L]:~0,5!%"=="Reply" (
+    if "!pingarray[%%L]:~0,5!"=="!replies[%defaultLang%]!" (
         set response=!pingarray[%%L]!
-    ) else if "!pingarray[%%L]:~0,4!%"=="PING" (
+    ) else if "!pingarray[%%L]:~0,4!"=="PING" (
         set response=!pingarray[%%L]!
         echo !response! - %time:~0,-3%
         if "!executefrom!"=="cmd" (
-            goto END
+            goto :eof
         ) else (
             set ip=
             echo Please try again
             goto FILE
         )
-    ) else if "!pingarray[%%L]:~0,12!%"=="Ping request" (
+    ) else if "!pingarray[%%L]:~0,12!"=="!pingRequests[%defaultLang%]!" (
         set response=!pingarray[%%L]!
         echo !response! - %time:~0,-3%
         if "!executefrom!"=="cmd" (
-            goto END
+            goto :eof
         ) else (
             set ip=
             echo Please try again
             goto FILE
         )
-    ) else if "!pingarray[%%L]:~0,7!%"=="Request" (
+    ) else if "!pingarray[%%L]:~0,7!"=="!Requests[%defaultLang%]!" (
         set response=!pingarray[%%L]! 
     ) else (
         echo Unused lines. > NUL
@@ -153,7 +232,6 @@ for /l %%L in (1,1,%LineCount%) do (
 
 
 rem =========== Writes the ping reply to logfile ================================================
-set response=%response:time<=time=%
 echo %response% - %time:~0,-3%>> "%filename%"
 
 
@@ -161,19 +239,44 @@ rem =========== Reads the last line from the logfile ===========================
 for /f "tokens=*" %%L in (%filename%) do ( 
     set line=%%L 
 )
+
+
 echo %line%
 
+
 ping 224.0.0.0 -n 1 -w 1400 >NUL
+
 
 goto LOOP
 
 
 
 
+
+
 rem ============================================================================================
-rem End
+rem ============================================================================================
+rem = Functions
+rem ============================================================================================
 rem ============================================================================================
 
-:END
+
+:strlen <resultVar> <stringVar>
+(   
+    (set^ tmp=!%~2!)
+    if defined tmp (
+        set "len=1"
+        for %%P in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (
+            if "!tmp:~%%P,1!" NEQ "" ( 
+                set /a "len+=%%P"
+                set "tmp=!tmp:~%%P!"
+            )
+        )
+    ) ELSE (
+        set len=0
+    )
+)
+
+
 
 
